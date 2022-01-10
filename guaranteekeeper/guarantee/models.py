bbd2f10de2a9108a2d7f861a65537f1d66ea49bb
Seungho Jeong <sh007.jeong@gmail.com>
2022-01-10 19:35:40 +0900

    modify: 금액 validator 변경 및 일부 이미지 null true 반영
    - 구입금액 양수 검증을 기존 is_positive_number 메서드 방식에서 django
      기본 필드(PositiveIntegerField)로 변경.
    
    - 제품 사진(product_image)과 영수증 사진(receipt_image)를 필수에서 비필
      수  항목으로 변경.

--- guaranteekeeper/guarantee/models.py
+++ guaranteekeeper/guarantee/models.py
@@ -12,12 +12,6 @@ def get_image_upload_path(instance, filename):
         raise ValidationError('`%s` is not a supported file extension.' % filename)
 
 
-def is_positive_number(number):
-    if number < 0:
-        raise ValidationError('Only positive number can be entered. (`%d` is not a positive number.)' % number)
-    return number
-
-
 class TimeStamped(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -31,11 +25,11 @@ class Guarantee(TimeStamped):
     model_code = models.CharField(max_length=30, blank=True)
     serial_number = models.CharField(max_length=40, blank=True, default='-')
     purchased_date = models.DateTimeField(blank=True, null=True)
-    paid_money = models.IntegerField(validators=[is_positive_number], blank=True, null=True)
+    paid_money = models.PositiveIntegerField(default=0)
     warranty = models.DateTimeField(blank=True, null=True)
     warranty_image = models.ImageField(upload_to=get_image_upload_path)
-    product_image = models.ImageField(upload_to=get_image_upload_path)
-    receipt_image = models.ImageField(upload_to=get_image_upload_path)
+    product_image = models.ImageField(upload_to=get_image_upload_path, blank=True, null=True)
+    receipt_image = models.ImageField(upload_to=get_image_upload_path, blank=True, null=True)
 
     def __str__(self):
         return self.product
